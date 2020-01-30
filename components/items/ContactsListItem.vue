ff22d8e074685f9128ce0f17342a1e437967e53e
Appisian <thanhbinhtang.freelance@gmail.com>
2020-01-30 23:52:33 +0100

    Create Contacts List Item

--- components/items/ContactsListItem.vue
+++ components/items/ContactsListItem.vue
@@ -1,14 +1,70 @@
 <template>
-  <div class="contact"></div>
+  <nuxt-link :to="route" class="contact">
+    <div class="pic"></div>
+    <div class="info">
+      <h1 v-text="fullname" />
+    </div>
+  </nuxt-link>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    contact: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    fullname() {
+      return `${this.contact.firstname} ${this.contact.lastname}`;
+    },
+    route() {
+      return {
+        name: this.contact.fullname,
+        path: `user/${this.contact.id}`,
+      };
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
-.contact {
-  height: 80px; width: 100%;
-  background-color: red;
-}
+.contact
+  height 80px
+  width 100%
+  border-bottom 1px solid $color-primary
+  display flex
+  align-items center
+  position relative
+  text-decoration none
+  color inherit
+
+  &:after
+    content ''
+    position absolute
+    top 50%
+    right 20px
+    transform-origin center center
+    transform rotate(45deg) translateY(-50%)
+    border-top 1px solid $color-fifth
+    border-right 1px solid $color-fifth
+    height 8px
+    width @height
+
+  .pic
+    background-color lightblue
+    border-radius 50%
+    width 40px
+    height @width
+    margin-right 30px
+
+  .info
+    h1
+      font-size $font-size-x-basic
+      color #34495e
+
+    p
+      margin 5px 0 0 0
+      color $color-fifth
 </style>
---
+++ store/index.js
@@ -0,0 +1,11 @@
+export const state = () => ({
+  contacts: [
+    {
+      id: 0,
+      firstname: 'Amélie',
+      lastname: 'Darouechi',
+      phone: '+33333333333',
+      email: 'aze@aze.com',
+    },
+  ],
+});
