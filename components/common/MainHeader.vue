db9b4029aa0312948f420753c10b5bc95917d1c8
Appisian <thanhbinhtang.freelance@gmail.com>
2020-01-30 22:46:19 +0100

    Create Main Header to show name

---
+++ components/common/MainHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="main-header"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped>
+.main-header {
+  height: 60px; width: 100%;
+  border-bottom: 1px solid $color-primary;
+}
+</style>
