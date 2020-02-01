93c255ef22b31995c46123dc7722b94ad55decd8
Appisian <thanhbinhtang.freelance@gmail.com>
2020-02-01 12:39:18 +0100

    Change main background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@ export default {
 
 <style lang="stylus" scoped>
 .__container {
-  background-image: linear-gradient(to right top, #d16ba5, #c777b9, #ba83ca, #aa8fd8, #9a9ae1, #8aa7ec, #79b3f4, #69bff8, #52cffe, #41dfff, #46eefa, #5ffbf1);
+  background-color: $color-background-primary;
   display: flex;
   justify-content: center;
   align-items: center;
