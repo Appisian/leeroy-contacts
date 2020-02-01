1373aff7d7baffc3f1df7801c55c91623af1d9fe
Appisian <thanhbinhtang.freelance@gmail.com>
2020-02-01 12:49:25 +0100

    Add new fonts

--- plugins/webfontloader.js
+++ plugins/webfontloader.js
@@ -1,7 +1,7 @@
 const WebFont = require("webfontloader"); //eslint-disable-line
 WebFont.load({
   google: {
-    families: ['Amaranth:400,700', 'Open+Sans:400,400i,600,700, sans-serif'],
+    families: ['Montserrat:400,500,600', 'Lato:400,700'],
   },
   timeout: 1000, // Set the timeout to two seconds
 });
